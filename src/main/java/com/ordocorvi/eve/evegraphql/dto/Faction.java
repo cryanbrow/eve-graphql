eb15e1477b195b02a55322158c1ce20c7a94d951
cryanbrow <cryan.brow@gmail.com>
2019-08-24 21:06:10 -0500

    Working on Json Propreties still

--- src/main/java/com/ordocorvi/eve/evegraphql/dto/Faction.java
+++ src/main/java/com/ordocorvi/eve/evegraphql/dto/Faction.java
@@ -1,5 +1,7 @@
 package com.ordocorvi.eve.evegraphql.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -9,14 +11,29 @@ import lombok.ToString;
 
 @NoArgsConstructor @AllArgsConstructor @Getter @Setter @ToString @EqualsAndHashCode
 public class Faction {
-	private Corporation corporation;
+	@JsonProperty("corporation_id")
+	private long corporationId;
+	@JsonProperty("description")
 	private String description;
-	private Faction faction;
-	private boolean is_unique;
-	private Corporation militia_corporation;
+	@JsonProperty("faction_id")
+	private long factionId;
+	@JsonProperty("is_unique")
+	private boolean isUnique;
+	@JsonProperty("militia_corporation_id")
+	private long militiaCorporationId;
+	@JsonProperty("name")
 	private String name;
-	private float size_factor;
-	private com.ordocorvi.eve.evegraphql.dto.System solar_system;
-	private int station_count;
-	private int station_system_count;
+	@JsonProperty("size_factor")
+	private float sizeFactor;
+	@JsonProperty("solar_system_id")
+	private long solarSystemId;
+	@JsonProperty("station_count")
+	private int stationCount;
+	@JsonProperty("station_system_count")
+	private int stationSystemCount;
+	
+	//Non Json Values
+	private Corporation corporation;
+	private Corporation militiaCorporation;
+	private com.ordocorvi.eve.evegraphql.dto.System solarSystem;
 }
---
+++ src/main/java/com/ordocorvi/eve/evegraphql/resolvers/StationResolver.java
@@ -0,0 +1,24 @@
+package com.ordocorvi.eve.evegraphql.resolvers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.coxautodev.graphql.tools.GraphQLResolver;
+import com.ordocorvi.eve.evegraphql.dao.CrestDao;
+import com.ordocorvi.eve.evegraphql.dto.Corporation;
+import com.ordocorvi.eve.evegraphql.dto.Station;
+
+import lombok.ToString;
+
+@ToString @Component
+public class StationResolver implements GraphQLResolver<Station> {
+
+	@Autowired
+	private CrestDao crestDao;
+	
+	/*
+	 * public Corporation getOwner_corporation(Station station) { return
+	 * crestDao.getCorporationById(station.getOwner()); }
+	 */
+	
+}
